ec1adf6c56903e1bdd90566a649da585d9686aa5
Jean-michel Dechamps <jean-michel-62@orange.fr>
2011-06-28 09:29:23 -0700

    Add translation file for french

---
+++ po/hamfax_fr.ts
@@ -0,0 +1,438 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>CorrectDialog</name>
+    <message>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Annuler</translation>
+    </message>
+</context>
+<context>
+    <name>FaxWindow</name>
+    <message>
+        <source>lpm</source>
+        <translation>LPM</translation>
+    </message>
+    <message>
+        <source>mono</source>
+        <translation>Mono</translation>
+    </message>
+    <message>
+        <source>wide</source>
+        <translation>large</translation>
+    </message>
+    <message>
+        <source>&amp;Exit</source>
+        <translation>&amp;Sortie</translation>
+    </message>
+    <message>
+        <source>&amp;File</source>
+        <translation>&amp;Fichier</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>&amp;Aide</translation>
+    </message>
+    <message>
+        <source>&amp;Open</source>
+        <translation>&amp;Ouvrir</translation>
+    </message>
+    <message>
+        <source>&amp;Save</source>
+        <translation>&amp;Sauvegarder</translation>
+    </message>
+    <message>
+        <source>device settings</source>
+        <translation>&amp;Réglages périphérique</translation>
+    </message>
+    <message>
+        <source>color</source>
+        <translation>couleur</translation>
+    </message>
+    <message>
+        <source>length of the black/white pattern at the end of a facsimile</source>
+        <translation>Longueur des motifs noirs et blancs à la fin d'un fax</translation>
+    </message>
+    <message>
+        <source>automatic scroll to last received line</source>
+        <translation>Défilement automatique à la dernière ligne reçue</translation>
+    </message>
+    <message>
+        <source>phasing lines mark the beginning of a line and the speed (lpm)</source>
+        <translation>Les lignes de synchro déterminent le début des lignes et la vitesse en LPM</translation>
+    </message>
+    <message>
+        <source>modulation settings</source>
+        <translation>Réglages de modulation</translation>
+    </message>
+    <message>
+        <source>Please enter IOC</source>
+        <translation>Entrez l'index d'alignement (IOC): </translation>
+    </message>
+    <message>
+        <source>&amp;Image</source>
+        <translation>&amp;Imaqes</translation>
+    </message>
+    <message>
+        <source>In color mode each line is split in three lines: red, green and blue.</source>
+        <translation>En mode couleur, chaque ligne est triplée en lignes rouges,vertes,et bleues</translation>
+    </message>
+    <message>
+        <source>select beginning of line</source>
+        <translation>Sélectionnez le début de ligne</translation>
+    </message>
+    <message>
+        <source>length of the black/white pattern at the beginning of a facsimile</source>
+        <translation>Longueur du motif noir et blanc au début du fax</translation>
+    </message>
+    <message>
+        <source>signal carrier for FM and AM</source>
+        <translation>Fréquence de la porteuse AM et FM</translation>
+    </message>
+    <message>
+        <source>Receive from d&amp;sp</source>
+        <translation>Reception depuis la &amp;Carte son</translation>
+    </message>
+    <message>
+        <source>Receive from P&amp;TC</source>
+        <translation>Reception depuis &amp;PTC</translation>
+    </message>
+    <message>
+        <source>input file name</source>
+        <translation>Nom du fichier d'entrée</translation>
+    </message>
+    <message>
+        <source>output file name</source>
+        <translation>Nom du fichier de sortie</translation>
+    </message>
+    <message>
+        <source>inverted</source>
+        <translation>inversé</translation>
+    </message>
+    <message>
+        <source>redraw as mono facsimile</source>
+        <translation>Redessine en fax mono</translation>
+    </message>
+    <message>
+        <source>load image</source>
+        <translation>Charger une image</translation>
+    </message>
+    <message>
+        <source>normal means 2.5% white, 95% black and again 2.5% white</source>
+        <translation>La norme est 2.5% blanc, 95% noir et encore 2.5% blanc</translation>
+    </message>
+    <message>
+        <source>apt start</source>
+        <translation>Début d'APT</translation>
+    </message>
+    <message>
+        <source>&amp;Options</source>
+        <translation>&amp;Options</translation>
+    </message>
+    <message>
+        <source>&amp;Scale to IOC (scale whole image)</source>
+        <translation>M&amp;Mise à l'échelle IOC (image complète)</translation>
+    </message>
+    <message>
+        <source>Receive from f&amp;ile</source>
+        <translation>Reçoit depuis un &amp;fichier</translation>
+    </message>
+    <message>
+        <source>set beginning of line</source>
+        <translation>Règler le début de ligne</translation>
+    </message>
+    <message>
+        <source>About &amp;QT</source>
+        <translation>A Propos de &amp;QT</translation>
+    </message>
+    <message>
+        <source>filter</source>
+        <translation>Filtre</translation>
+    </message>
+    <message>
+        <source>middle</source>
+        <translation>milieu</translation>
+    </message>
+    <message>
+        <source>narrow</source>
+        <translation>Etroite</translation>
+    </message>
+    <message>
+        <source>apt stop</source>
+        <translation>Fin de APT</translation>
+    </message>
+    <message>
+        <source>save image</source>
+        <translation>Sauvegarder l'image</translation>
+    </message>
+    <message>
+        <source>redraw as color facsimile</source>
+        <translation>Redessiner come fax couleur</translation>
+    </message>
+    <message>
+        <source>key PTT while transmitting with DSP</source>
+        <translation>Commutation PTT lors d'une transmission avec DSP</translation>
+    </message>
+    <message>
+        <source>frequency of the black/white pattern at the end of a facsimile</source>
+        <translation>Fréquence des motifs noirs et blancs à la fin d'un fax</translation>
+    </message>
+    <message>
+        <source>&amp;Transmit</source>
+        <translation>&amp;Emetre</translation>
+    </message>
+    <message>
+        <source>&amp;Receive</source>
+        <translation>&amp;Recevoir</translation>
+    </message>
+    <message>
+        <source>&amp;select font</source>
+        <translation>&amp;Sélection de police</translation>
+    </message>
+    <message>
+        <source>HamFax is a Qt application for transmitting and receiving am radio facsimiles and weather broadcasts.
+Author: Christof Schmitt, DH1CS &lt;cschmitt@users.sourceforge.net&gt;
+
+The demodulator is taken from ACfax by Andreas Czechanowski, DL4SDC
+
+License: GNU General Public License
+Version: %1</source>
+        <translation>HamFax est une application QT destinée à la réception et àn l'emmission de fax radios et des fax météos.
+Auteur: Cristof Schmitt, DH1CS &lt;cschmitt@users.sourceforge.net&gt;
+
+Le démodulateur est tiré d'ACfax par  Andreas Czechanowski, DL4SDC
+License : GNU General Public License
+
+Version: %1</translation>
+    </message>
+    <message>
+        <source>&amp;Quick save as PNG</source>
+        <translation>S&amp;Sauvegarde rapide comme PNG</translation>
+    </message>
+    <message>
+        <source>deviation for FM</source>
+        <translation>Déviation FM</translation>
+    </message>
+    <message>
+        <source>carrier</source>
+        <translation>Porteuse</translation>
+    </message>
+    <message>
+        <source>facsimile settings</source>
+        <translation>&amp;Réglages FAX</translation>
+    </message>
+    <message>
+        <source>apt settings</source>
+        <translation>Réglages APT</translation>
+    </message>
+    <message>
+        <source>show tool tips</source>
+        <translation>Montrer les astuces</translation>
+    </message>
+    <message>
+        <source>shift colors (R-&gt;G,G-&gt;B,B-&gt;R)</source>
+        <translation>Remapper les couleurs (rouge-vert,vert-bleu,bleu-rouge)</translation>
+    </message>
+    <message>
+        <source>image size: %1x%2</source>
+        <translation>Taille d'image: %1x%2</translation>
+    </message>
+    <message>
+        <source>Transmit to &amp;file</source>
+        <translation>Emettre dans le &amp; fichier</translation>
+    </message>
+    <message>
+        <source>lines per minute</source>
+        <translation>Lignes par minute</translation>
+    </message>
+    <message>
+        <source>select second point of vertical line</source>
+        <translation>Cliquer sur le second point de la ligne verticale</translation>
+    </message>
+    <message>
+        <source>modulation</source>
+        <translation>Modulation</translation>
+    </message>
+    <message>
+        <source>frequency of the black/white pattern
+at the beginning of a facsimile</source>
+        <translation>Fréquence des motifs noirs et blancs au début d'un fax</translation>
+    </message>
+    <message>
+        <source>deviation</source>
+        <translation>Déviation</translation>
+    </message>
+    <message>
+        <source>AM is only used for getting images
+from weather satellites together with
+a FM receiver. FM together with a
+USB (upper side band) transceiver is
+the right setting for HF</source>
+        <translation>L'AM est seulement utilisée pour récupérer des images de
+satellites météos avec un récepteur FM.La FM avec un récepteur
+règlé en USB est utilisée en HF (bandes décamétriques)</translation>
+    </message>
+    <message>
+        <source>&amp;Adjust IOC (change width)</source>
+        <translation>&amp;Ajuster l'index d'alignement  (changer la largeur)</translation>
+    </message>
+    <message>
+        <source>Index Of Cooperation:
+image width in pixels divided by PI</source>
+        <translation>Index +d'alignement(IOC): largeur d'image en pixels divisée par PI</translation>
+    </message>
+    <message>
+        <source>bandwidth of the software demodulator</source>
+        <translation>Bande passante du démodulateur logiciel</translation>
+    </message>
+    <message>
+        <source>slant correction</source>
+        <translation>Correction de pente </translation>
+    </message>
+    <message>
+        <source>select first point of vertical line</source>
+        <translation>Cliquer sur le premier point d'une ligne verticale</translation>
+    </message>
+    <message>
+        <source>IOC: %1</source>
+        <translation>IOC: %1</translation>
+    </message>
+    <message>
+        <source>&amp;Don&apos;t Exit</source>
+        <translation>&amp;Ne pas sortir</translation>
+    </message>
+    <message>
+        <source>&amp;About HamFax</source>
+        <translation>&amp;A propos d'HamFax</translation>
+    </message>
+    <message>
+        <source>Really exit?</source>
+        <translation>Réellement sortir ?</translation>
+    </message>
+    <message>
+        <source>Transmit using &amp;PTC</source>
+        <translation>Emettre avec le &amp;PTC</translation>
+    </message>
+    <message>
+        <source>Transmit using &amp;dsp</source>
+        <translation>Emettre avec &amp;La carte son</translation>
+    </message>
+    <message>
+        <source>phasing lines</source>
+        <translation>Lignes de synchronisation</translation>
+    </message>
+</context>
+<context>
+    <name>File</name>
+    <message>
+        <source>could not allocate AFfilesetup</source>
+        <translation>Ne peut allouer AFfilesetup</translation>
+    </message>
+    <message>
+        <source>samples size not 16 bit</source>
+        <translation>Lex échantillons ne sont pas sur 16 bits</translation>
+    </message>
+    <message>
+        <source>could not open file</source>
+        <translation>Impossible d'ouvrir le fichier</translation>
+    </message>
+    <message>
+        <source>sample rate is not 8000 Hz</source>
+        <translation>La fréquence d'échantillonnage n'est pas de 8000 Hz</translation>
+    </message>
+</context>
+<context>
+    <name>HelpDialog</name>
+    <message>
+        <source>&amp;Close</source>
+        <translation>&amp;Fermer</translation>
+    </message>
+</context>
+<context>
+    <name>OptionsDialog</name>
+    <message>
+        <source>ptc speed</source>
+        <translation>Vitesse PTC</translation>
+    </message>
+    <message>
+        <source>dsp device</source>
+        <translation>Carte son</translation>
+    </message>
+    <message>
+        <source>ptt device</source>
+        <translation>Voie PTT</translation>
+    </message>
+    <message>
+        <source>ptc device</source>
+        <translation>Voie PTC</translation>
+    </message>
+    <message>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Annuler</translation>
+    </message>
+</context>
+<context>
+    <name>ReceiveDialog</name>
+    <message>
+        <source>phasing line, lpm %1</source>
+        <translation>Synchronisation, %1 LPM</translation>
+    </message>
+    <message>
+        <source>skip phasing</source>
+        <translation>&amp;Eviter la synchronisation</translation>
+    </message>
+    <message>
+        <source>receiving line %1</source>
+        <translation>Réception de la ligne %1</translation>
+    </message>
+    <message>
+        <source>decoding phasing</source>
+        <translation>Décodage signaux de synchronisation</translation>
+    </message>
+    <message>
+        <source>Apt frequency: %1 Hz</source>
+        <translation>Fréquence APT: %1 Hz</translation>
+    </message>
+    <message>
+        <source>&amp;Skip apt start</source>
+        <translation>&amp;Eviter l'atentte de signal APT</translation>
+    </message>
+    <message>
+        <source>searching APT start tone</source>
+        <translation>Atentte de signal APT</translation>
+    </message>
+    <message>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Annuler</translation>
+    </message>
+</context>
+<context>
+    <name>Sound</name>
+    <message>
+        <source>could not open dsp device</source>
+        <translation>Impossible d'ouvrir la carte son</translation>
+    </message>
+    <message>
+        <source>could not set audio format to S16_NE</source>
+        <translation>Impossible de forcer le format S16_NE</translation>
+    </message>
+    <message>
+        <source>could not set audio format S16_LE</source>
+        <translation>Impossible de forcer le format S16_LE</translation>
+    </message>
+    <message>
+        <source>could not set sample rate</source>
+        <translation>Impossible de forcer la fréquence d'échantillonage</translation>
+    </message>
+    <message>
+        <source>could not set mono mode</source>
+        <translation>Impossible de passer en mono</translation>
+    </message>
+</context>
+<context>
+    <name>TransmitDialog</name>
+    <message>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Annuler</translation>
+    </message>
+</context>
+</TS>
